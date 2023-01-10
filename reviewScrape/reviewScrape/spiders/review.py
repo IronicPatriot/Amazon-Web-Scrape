5466f8a6cf332d55f10a6110ea9185104df51c2c
Liam Green <imlgg@yahoo.co.uk>
2023-01-10 21:04:18 +0000

    first version of review scrape

--- reviewScrape/reviewScrape/spiders/review.py
+++ reviewScrape/reviewScrape/spiders/review.py
@@ -8,5 +8,18 @@ class ReviewSpider(scrapy.Spider):
         start_urls = 'https://www.amazon.co.uk/God-of-War-Ragnar%C3%B6k-PS5/dp/B0B6FGSKCQ/ref=sr_1_1?keywords=god+of+war+ragnarok+ps5&qid=1673294296&sprefix=god+of+w%2Caps%2C117&sr=8-1'
         yield SplashRequest(url=start_urls, callback=self.parse)
 
-    def parse(self, response):
-        pass
+    def parse(self, response): # #cm-cr-dp-review-list
+        review = response.css('div[data-hook=review]')
+        for item in review:
+            yield{
+                'USERNAME': item.css('div:nth-child(1)>a>div.a-profile-content>span::text').get(),
+                "rating": item.css("*[data-hook*=review-star-rating] ::text").re(r"(\d+\.*\d*) out"),
+                # from example code, remove zero to get all
+                'TEXT': item.css("span[data-hook=review-body] ::text").getall(),
+                # has to be a getall or just gets the first line of review
+
+            }
+
+os.system("scrapy crawl review -O test-two.json")
+
+# remove junk from text get
--- reviewScrape/reviewScrape/spiders/search.py
+++ reviewScrape/reviewScrape/spiders/search.py
@@ -17,7 +17,7 @@ class SearchSpider(scrapy.Spider):
         start_urls = 'https://www.amazon.co.uk/s?k=' + userInput
         # start_urls = 'https://www.amazon.co.uk/s?k=god+of+war+ragnarok+ps5&sprefix=go%2Caps%2C119&ref=nb_sb_ss_ts-doa-p_1_2'
         yield SplashRequest(url=start_urls, callback=self.parse)
-        # yield scrapy.Request (when not using splash)
+        # yield scrapy.Request #when not using splash
 
     def parse(self, response):
         product = response.css('div.s-result-item[data-component-type=s-search-result]')
@@ -31,15 +31,14 @@ class SearchSpider(scrapy.Spider):
 
 os.system("scrapy crawl search -O test.json")
 
-# build second spider for review page
+# finish
 # then combine
 
 # #customer_review-R33A3MQMORZ8OW
 
 #customer_review-R33A3MQMORZ8OW>div.a-row.a-spacing-small.review-data
 
-# https://www.amazon.co.uk/God-of-War-Ragnar%C3%B6k-PS5/dp/B0B6FGSKCQ/ref=sr_1_1?keywords=god%2Bof%2Bwar%2Bragnarok%2Bps5&qid=1673294296&sprefix=god%2Bof%2Bw%2Caps%2C117&sr=8-1&th=1
 
 # response.css("span[data-hook=review-body] ::text").getall()
 
-# get just gives a bunch of "nnnnn", getall gets all reviews plus that garbage, so partly working
+#cm-cr-dp-review-list
